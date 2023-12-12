7496d4a809a575eef51ee4aaa505936eb2569f98
Brandon <bmcomis2018@gmail.com>
2023-12-12 00:44:22 -0600

    Begin modeling index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,76 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<div class="hero">
+  <h1>Hey, I'm Brandon</h1>
+  <h2>(also known as SSS-Says-Snek)</h2>
+</div>
+
+<div class="cards">
+</div>
+
+
+<style>
+  :global(:root) {
+    --clr-base: #1e1e2e;
+    --clr-text: #cdd6f4;
+    --clr-accent: #b4befe;
+    --clr-mauve: #cba6f7;
+    --clr-black: #11111b;
+  }
+
+  :global(body) {
+    color-scheme: dark;
+    font-family: 'Montserrat';
+
+    background-color:var(--clr-base);
+    color: var(--clr-text);
+  }
+
+  h1, h2 {
+    margin: 0;
+  }
+
+  h1 {
+    font-size: 5em;
+  }
+
+  h2 {
+    font-size: 1.25em;
+  }
+
+  .hero {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+    padding: 4rem 0;
+
+    background:
+        linear-gradient(45deg, var(--clr-mauve), var(--clr-accent));
+    color: var(--clr-black);
+    animation: hero 3s forwards;
+  }
+
+  @keyframes hero {
+    from {
+      opacity: 0;
+      transform: translateY(-100px);
+      color: var(--clr-text);
+    }
+
+    30% {
+      transform: translateY(0px);
+      color: var(--clr-text);
+    }
+
+    60% {
+      opacity: 1;
+      color: var(--clr-black);
+    }
+
+    to {
+      opacity: 0.5;
+    }
+  }
+</style>
