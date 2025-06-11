3a003a88c30c94465cd0f64d1a6afc9d00e904c5
Brandon <bmcomis2018@gmail.com>
2025-06-11 16:10:31 -0500

    Remove console log

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,5 +2,4 @@ import { createClient } from '@supabase/supabase-js'
 
 import { SUPABASE_URL, SUPABASE_KEY } from '$env/static/private';
 
-console.log(SUPABASE_URL, SUPABASE_KEY);
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
